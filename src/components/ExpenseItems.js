2e8892ebece64db2c4807402d0ee6a7629579137
devangga <devanggakertawijaya@gmail.com>
2022-08-04 13:27:56 +0700

    fixing date method

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -3,13 +3,15 @@ import './ExpenseItems.css';
 function ExpenseItem(props) {
 
     const month = props.date.toLocaleString('en-US', {month: 'long'})
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'})
+    const year = props.date.getFullYear();
 
   return (
     <div className='expense-item'>
       <div>
         <div>{month}</div>
-        <div>YEAR</div>
-        <div>DAY</div>
+        <div>{year}</div>
+        <div>{day}</div>
       </div>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
