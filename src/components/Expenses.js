bca81f233a85a703f417c3f5a3286d7c6f6f7991
devangga <devanggakertawijaya@gmail.com>
2022-09-06 14:28:58 +0700

    import React

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,15 +1,17 @@
+import React from "react"
+import Card from "./Card"
 import ExpenseItems from "./ExpenseItems"
 import './Expenses.css'
 
 function Expenses (props) {
     return (
-        <div className="expenses">
+        <Card className="expenses">
             <ExpenseItems title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date}/>
             <ExpenseItems title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date}/>
             <ExpenseItems title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date}/>
             <ExpenseItems title={props.expenses[3].title} amount={props.expenses[3].amount} date={props.expenses[3].date}/>
-        </div>
+        </Card>
     )
 }
 
-export default Expenses
+export default Expenses 
