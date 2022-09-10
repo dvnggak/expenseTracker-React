043d7cc8d2aeb7abfa05f5b9c287a1312269169d
devangga <devanggakertawijaya@gmail.com>
2022-09-10 20:07:20 +0700

    component for form

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./ExpenseForm.css"
+
+const NewExpenseForm = () => {
+    return (
+        <div>
+
+        </div>
+    )
+};
+
+export default NewExpenseForm;
