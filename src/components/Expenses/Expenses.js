03ae75eadc21160a68abfca28807fce9a8d33176
devangga <devanggakertawijaya@gmail.com>
2022-09-16 10:35:53 +0700

    pass data to expense.js

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,21 @@
-import React from "react"
-import ExpensesFilter from "../ExpenseFilter/ExpenseFilter"
+import React, { useState } from "react"
+import ExpensesFilter from "../ExpenseFilter/ExpensesFilter"
 import Card from "../UI/Card"
 import ExpenseItems from "./ExpenseItems"
 import './Expenses.css'
 
 const Expenses = (props) => {
+
+    const [filteredYear, setFilteredYear] = useState('2020')
+
+    const selectedYearFilter = (selectedYear) => {
+        setFilteredYear(selectedYear)
+        console.log('In Expense.js', selectedYear)
+    }
+
     return (
         <Card className="expenses">
-            <ExpensesFilter />
+            <ExpensesFilter initialSelect={filteredYear} onSelectedYearFilter={selectedYearFilter}/>
             <ExpenseItems title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date}/>
             <ExpenseItems title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date}/>
             <ExpenseItems title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date}/>
