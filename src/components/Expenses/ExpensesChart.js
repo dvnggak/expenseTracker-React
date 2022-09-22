7cf693659d4d9e65f1bad7a5a23558f6113b202b
devangga <devanggakertawijaya@gmail.com>
2022-09-22 11:42:30 +0700

    fix error on for in should be for of

--- src/components/Expenses/ExpensesChart.js
+++ src/components/Expenses/ExpensesChart.js
@@ -54,7 +54,7 @@ const ExpensesChart = (props) => {
         },
     ]
 
-    for (const expense in props.expenses) {
+    for (const expense of props.expenses) {
         const expenseMonth = expense.date.getFullMonth();
         chartDataPoints[expenseMonth].value += expense.amount
     }
